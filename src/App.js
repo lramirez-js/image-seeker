77ad7998173b32a18b66f22c10a00642b71ee3d7
lramirez-js <luis.rg@outlook.com>
2022-09-13 13:20:18 -0500

    Added header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Field, Form, Formik } from 'formik'
+import './header.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App = () => {
+  return(
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: '' }}
+          onSubmit={ async values => {
+            console.log()
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name="search" />
+          </Form>
+        </Formik>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
