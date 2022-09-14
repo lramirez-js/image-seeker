c2340f3bcf0eaf74a5be037828fbb5b4bfe4c1da
lramirez-js <luis.rg@outlook.com>
2022-09-13 20:14:01 -0500

    Add fetch and HTML

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import { useState } from 'react'
 import { Field, Form, Formik } from 'formik'
 import './header.css'
 
 const App = () => {
+  const [photos, setPhotos] = useState([]);
+  const open = url => window.open(url)
+
   return(
     <div>
       <header>
         <Formik
           initialValues={{ search: '' }}
           onSubmit={ async values => {
-            console.log()
+            const response = await fetch(
+              `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
+                headers: {
+                  'Authorization': 'Client-ID iPuChME4emWrCk9wccPJp79gSCNHdbbaDg_Tk2PDW2k'
+                }
+              }
+              
+            )
+            const data = await response.json()
+            setPhotos(data.results)
           }}
         >
           <Form>
@@ -16,6 +29,15 @@ const App = () => {
           </Form>
         </Formik>
       </header>
+      <div className='container'>
+        <div className='center'>
+          {photos.map(photo => 
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular} />
+              <p>{[photo.description, photo.alt_description].join(' -  ')}</p>
+            </article>)}
+        </div>
+      </div>
     </div>
   )
 }
